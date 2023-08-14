d3f533a0a24635a2e9056543a63ea07b365589d5
jje005 <jje00500@naver.com>
2023-08-14 16:08:10 +0900

    MVC적용

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import {STOP_SIN_DATA} from '../constants.js';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine } from 'recharts';
+import styles from './module.css';
 
 const electron = window.require("electron")
 
@@ -24,22 +26,23 @@ class LineChartComponent extends Component {
 
   drawSinGraph = () => {
     const intervalId = setInterval(() => {
-      const { dataCount, chartData, referenceLines } = this.state;
+      const {chartData, referenceLines } = this.state;
       const { sinData } = this.props;
+      let dataCount = this.state.dataCount;
 
       const xValue = sinData.name;
       const yValue = sinData.uv;
 
       this.setState({
         chartData: [...chartData, { name: xValue, uv: yValue }],
-        dataCount: dataCount + 1,
+        dataCount: dataCount++,
       });
 
-      if (chartData.length > 40) {
+      if (chartData.length > 10) {
         chartData.shift();
       }
 
-      if (dataCount % 100 === 0 && dataCount != 0) { // 주기별로 참조선 배열 업데이트
+      if (dataCount % 1000 === 0 && dataCount !== 0) {
         this.setState({
           referenceLines: [
             ...referenceLines,
@@ -47,7 +50,7 @@ class LineChartComponent extends Component {
           ],
         });
       }
-    }, 50);
+    }, 10);
 
     this.setState({
       intervalId: intervalId,
@@ -61,7 +64,7 @@ class LineChartComponent extends Component {
   };
 
   handleStopClick = () => {
-    electron.ipcRenderer.send('stop_sin', 'stop');
+    electron.ipcRenderer.send(STOP_SIN_DATA, 'stop');
     if (this.state.intervalId) {
       clearInterval(this.state.intervalId);
       this.setState({
@@ -74,139 +77,23 @@ class LineChartComponent extends Component {
     const { chartData, referenceLines } = this.state;
 
     return (
-      <div>
+      <div className={styles.container}>
         <h1>Sin 곡선 그래프</h1>
-        <LineChart width={620} height={400} data={chartData} syncId="sync">
-          <XAxis dataKey="name" />
-          <YAxis domain={[-127, 128]} ticks={[-127, -64, 0, 64, 128]} />
-          <CartesianGrid stroke="#ccc" />
-          <Tooltip />
-          <Legend />
-          {/* 1000ms 주기별 빨간 선을 동적으로 추가 */}
-          {referenceLines}
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        </LineChart>
-
-        <button id="startButton" onClick={this.handleStartClick} disabled={this.state.intervalId}>
-          시작
-        </button>
-        <button id="stopButton" onClick={this.handleStopClick} disabled={!this.state.intervalId}>
-          멈춰
-        </button>
-                <button id="homeButton" onClick={() => (window.location.href = './index.html')}>
-          첫페이지로
-        </button>
-      </div>
-    );
+        <div className={styles.lineChartContainer}>
+          <LineChart width={620} height={400} data={chartData} syncId="sync">
+            <XAxis dataKey="name" />
+            <YAxis domain={[-127, 128]} ticks={[-127, -64, 0, 64, 128]} />
+            <CartesianGrid stroke="#ccc" />
+            <Tooltip/>
+            <Legend />
+            {/* 1000ms 주기별 빨간 선을 동적으로 추가 */}
+            {referenceLines}
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          </LineChart>
+        </div>
+        </div>
+    )
   }
 }
 
 export default LineChartComponent;
-// import React, { Component } from 'react';
-// import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine} from 'recharts';
-
-// const electron = window.require("electron")
-
-// class LineChartComponent extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       intervalId: null,
-//       dataCount: 0,
-//       chartData: [],
-//     };
-//   }
-
-//   componentDidMount() {
-//     this.drawSinGraph();
-//   }
-
-//   componentWillUnmount() {
-//     this.handleStopClick(); // 컴포넌트가 언마운트될 때 중지하도록 수정
-//   }
-
-//   drawSinGraph = () => {
-//     const intervalId = setInterval(() => {
-//       const { dataCount, chartData } = this.state;
-//       const { sinData } = this.props;
-
-//       const xValue = sinData.name;
-//       const yValue = sinData.uv;
-//       var RedCount = 0;
-      
-
-
-//       this.setState({
-//         chartData: [...chartData, { name: xValue, uv: yValue }],
-//         dataCount: dataCount + 1,
-//       });
-
-//       if (chartData.length > 40) {
-//         chartData.shift();
-//       }
-
-//       if (chartData.length >= 20) { // 좌표 영역을 업데이트한다
-//         this.setState({
-//           startIndex: this.state.startIndex + 20,
-//           endIndex: this.state.endIndex + 20,
-//         });
-//       }
-
-//       if(dataCount % 100 === 0) {
-//         RedCount++;
-//         <ReferenceLine x={RedCount*100} label={`${chartData.length}ms`} stroke="red" />
-//       }; 
-//     }, 50);
-
-//     this.setState({
-//       intervalId: intervalId,
-//       referenceLines: [],
-//     });
-//   };
-
-//   handleStartClick = () => {
-//     if (!this.state.intervalId) {
-//       this.drawSinGraph();
-//     }
-//   };
-
-//   handleStopClick = () => {
-//     electron.ipcRenderer.send('stop_sin','stop');
-//     if (this.state.intervalId) {
-//       clearInterval(this.state.intervalId);
-//       this.setState({
-//         intervalId: null,
-//       });
-//     }
-//   };
-
-//   render() {  
-//     const { chartData } = this.state;
-
-//     return (
-//       <div>
-//         <h1>Sin 곡선 그래프</h1>
-//         <LineChart width={620} height={400} data={chartData} syncId="sync">
-//           <XAxis dataKey="name" />
-//           <YAxis domain={[-127, 128]} ticks={[-127, -64, 0, 64, 128]} />
-//           <CartesianGrid stroke="#ccc" />
-//           <Tooltip />
-//           <Legend />
-//           <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-//         </LineChart>
-
-//         <button id="startButton" onClick={this.handleStartClick} disabled={this.state.intervalId}>
-//           시작
-//         </button>
-//         <button id="stopButton" onClick={this.handleStopClick} disabled={!this.state.intervalId}>
-//           멈춰
-//         </button>
-//                 <button id="homeButton" onClick={() => (window.location.href = './index.html')}>
-//           첫페이지로
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default LineChartComponent;
--- src/main.js
+++ src/main.js
@@ -1,54 +1,81 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const { SEND_MAIN_PING, SET_SIN_VALUE } = require('./constants');
-const { clearInterval } = require('timers');
-
-let mainWindow;
-var count = 0;
-
+const { initMainModel } = require('./mainModel');
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
-      contextIsolation: false
-    }
+      contextIsolation: false,
+    },
   });
-  mainWindow.loadURL('http://localhost:3000');}
 
-ipcMain.on(SEND_MAIN_PING, (event, arg) => {
-  console.log("Main.js received a ping!!!");
-});
-
-let intervalId = null; // interval ID 저장 변수 추가
-
-ipcMain.on(SET_SIN_VALUE, (event, arg) => {
-  console.log("sin값 보내란다");
-  intervalId = setInterval(() => {
-    const data = {
-      name: ` ${(count*10)} ms`,
-      uv: Math.floor(Math.sin(count) * 128)
-    }    
-    mainWindow.webContents.send('periodic-data', data);
-    console.log(data.uv);
-  
-    count+=0.25;
-  }, 100)  
-})
-
-ipcMain.on('stop_sin', () => {
-  console.log("멈추라고 명령옴");
-  clearInterval(intervalId);
-  mainWindow.webContents.send('complete_sin', 'stop');
-});
+  mainWindow.loadURL('http://localhost:3000');
+  return mainWindow;
+}
 
 app.whenReady().then(() => {
-  createWindow();
+  const mainWindow = createWindow();
+  initMainModel(mainWindow);
 });
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
 });
+// const { app, BrowserWindow, ipcMain } = require('electron');
+// const path = require('path');
+// const { SEND_MAIN_PING, SET_SIN_VALUE,STOP_SIN_DATA, CYCLE_SIN_DATA } = require('./constants');
+// const { clearInterval } = require('timers');
+
+// let mainWindow;
+// var count = 0;
+
+
+// function createWindow() {
+//   mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: true,
+//       enableRemoteModule: true,
+//       contextIsolation: false
+//     }
+//   });
+//   mainWindow.loadURL('http://localhost:3000');}
+
+// ipcMain.on(SEND_MAIN_PING, (event, arg) => {
+//   console.log("Main.js received a ping!!!");
+// });
+
+// let intervalId = null; // interval ID 저장 변수 추가
+
+// ipcMain.on(SET_SIN_VALUE, (event, arg) => {
+//   console.log("sin값 요청");
+//   intervalId = setInterval(() => {
+//     const data = {
+//       name: ` ${(count)} ms`,
+//       uv: Math.sin(count*0.01) * 128
+//     }    
+//     mainWindow.webContents.send(CYCLE_SIN_DATA, data);
+//     console.log(data.uv);
+  
+//     count++;
+//   }, 10)  
+// })
+
+// ipcMain.on(STOP_SIN_DATA, () => {
+//   console.log("sin 데이터 생성 종료");
+//   clearInterval(intervalId);
+//   mainWindow.webContents.send(STOP_SIN_DATA , 'stop');
+// });
+
+// app.whenReady().then(() => {
+//   createWindow();
+// });
+
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') app.quit();
+// });
