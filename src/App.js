56365966bffa8488e9fcb7fd2a3f238f7536e77b
jje005 <jje00500@naver.com>
2023-08-23 09:06:45 +0900

    브랜치 test

--- src/App.js
+++ src/App.js
@@ -9,37 +9,22 @@ function App() {
   const [currentView, setCurrentView] = useState('main');
   const [sinData = [], setSinData] = useState([]); // sinData 상태 추가
 
-  const sendMain = () => {
-    electron.ipcRenderer.send(SEND_MAIN_PING, 'send');
-  }
 
-  const changeView = (viewName) => {
-    // electron.ipcRenderer.send(SET_SIN_VALUE, 'sin');
-    setCurrentView(viewName);
-  }
-
-  useEffect(() => {
-    electron.ipcRenderer.on(CYCLE_SIN_DATA, (event, data) => {
-      setSinData(data);
-    });
 
     return () => {
       electron.ipcRenderer.removeAllListeners(CYCLE_SIN_DATA);
     } ;
-  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-      {currentView === 'main' ? (
+      {currentView  (
           <React.Fragment>
             <p>그래프를 그려보자</p>
             <button size="large" onClick={sendMain}>Send Ping</button>
-            <button size="large" onClick={() => changeView('LineChartComponent')}>Sin 그래프</button>
           </React.Fragment>
-        ) : (
-          <LineChartComponent sinData={sinData} />
-        )}
+        )
+      }
       </header>
     </div>
   );
